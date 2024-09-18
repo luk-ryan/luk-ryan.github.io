2c52775e3744ec26f83819e1c8f1ed7d9564c8e3
luk-ryan <ryankluk@my.yorku.ca>
2024-09-18 17:15:25 -0400

    Added a Nav bar with working links

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">Ryan Luk</Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/about">About Me</Link>
+        </li>
+        <li>
+          <Link to="/Projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Me</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
