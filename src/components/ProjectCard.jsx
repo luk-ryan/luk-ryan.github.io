00c6af773ab9869e9da98c517ef4394491b2c6a7
luk-ryan <ryankluk@my.yorku.ca>
2024-10-30 18:44:36 -0400

    changed styling for laptop

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,6 +12,12 @@ const ProjectCard = ({ name, description, githubLink }) => {
     <div className="project-card">
       <div className="project-header">
         <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="project-links">
+        <Link className="button-link" to={`/projects/${formatTitle(name)}`}>
+          View Details
+        </Link>
         <a
           href={githubLink}
           target="_blank"
@@ -21,10 +27,6 @@ const ProjectCard = ({ name, description, githubLink }) => {
           <FontAwesomeIcon icon={faGithub} /> Source Code
         </a>
       </div>
-      <p>{description}</p>
-      <Link className="button-link" to={`/projects/${formatTitle(name)}`}>
-        View Details
-      </Link>
     </div>
   );
 };
