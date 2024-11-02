48bc8a12b35d44fb25b37cdc2eebe1ebaf32eefb
Ryan Luk <ryan.luk02@gmail.com>
2024-11-02 14:19:41 -0400

    Added project-based courses to the projects page

--- src/components/about-me/education/EECS3461.jsx
+++ src/components/about-me/education/EECS3461.jsx
@@ -13,19 +13,33 @@ const EECS3461 = () => {
         <ButtonBack link={"/about/education"} />
       </div>
       <div className="content">
-        <h2>Final Project</h2>
         <p>
           The biggest component of this course was to decide on a problem and
           come up with a design solution for this problem. As a team of four
           students, we researched problems by conducting surveys with potential
           end-users of a system as the first part of our project. After choosing
           a problem, we then brainstormed various ideas on a system that would
-          help solve this problem, drawing out Low-Fidelity prototypes, and
-          wireframes for it. Once we decided to move forward with the idea of a
-          mobile application, we used Figma to develop a working interactive
+          help solve this problem, drawing out Low-Fidelity prototypes.
+        </p>
+        <h2>Final Project</h2>
+        <p>
+          Once we decided to move forward with the idea of a mobile application,
+          we used Figma to develop wireframes, as well as a working interactive
           prototype and compiled it into a final report for submission.
         </p>
-        <h3>Report:</h3>
+        <div className="eecs3401-header">
+          <h3>Report:</h3>
+          <p>
+            <a
+              className="text-link"
+              href="https://www.figma.com/design/pYtrfKJOkf5Mg3Jd0xfMGd/EECS-3461"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Figma Design
+            </a>
+          </p>
+        </div>
         <iframe
           id="education/eecs 3461 - assignment 2.pdf"
           src="/education/eecs 3461 - assignment 2.pdf"
